cad99d0dec96bd21f781c08ee09da7d0c8f124af
peytonhobson <peyton.hobson@publicissapient.com>
2022-09-29 16:53:43 -0600

    Added font awesome as dependency

--- src/components/Home/DescriptionBox.vue
+++ src/components/Home/DescriptionBox.vue
@@ -1,1 +1,14 @@
-<template></template>
+<template>
+  <div class="w-full h-full border-black px-5 py-10">
+    <div><font-awesome-icon</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "DescriptionBox",
+  setup() {},
+});
+</script>
