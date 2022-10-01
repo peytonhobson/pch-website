8e0c723b97a88399a80377aad752d0c1055d7515
peytonhobson <peyton.hobson1@gmail.com>
2022-10-01 12:50:06 -0600

    Added padding to bottom of cards

--- src/views/FacilitiesView.vue
+++ src/views/FacilitiesView.vue
@@ -2,7 +2,7 @@
   <section class="text-6xl border-y-2 border-y-brand-green-gray py-4">
     Facilities
   </section>
-  <section v-if="!isMobile" class="grid grid-rows-2 mt-10">
+  <section v-if="!isMobile" class="grid grid-rows-2 my-10">
     <div class="row-start-1 row-span-1 flex justify-evenly">
       <display-card-baxter-house class="w-5/6 md:w-2/7 show-on-scroll" />
       <display-card-hallet-house class="w-5/6 md:w-2/7 show-on-scroll" />
@@ -13,8 +13,8 @@
       <display-card-trapper-house class="w-5/6 md:w-2/7 show-on-scroll" />
     </div>
   </section>
-  <section v-else class="flex flex-wrap justify-evenly">
-    <display-card-baxter-house class="w-5/6 md:w-2/7 show-on-scroll mt-5" />
+  <section v-else class="flex flex-wrap justify-evenly my-10">
+    <display-card-baxter-house class="w-5/6 md:w-2/7 show-on-scroll" />
     <display-card-hallet-house class="w-5/6 md:w-2/7 show-on-scroll mt-5" />
     <display-card-juneau-house class="w-5/6 md:w-2/7 show-on-scroll mt-5" />
     <display-card-madrona-house class="w-5/6 md:w-2/7 show-on-scroll mt-5" />
