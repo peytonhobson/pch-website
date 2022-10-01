82a407f5e9211175e53a8962762360dec6840179
peytonhobson <peyton.hobson1@gmail.com>
2022-10-01 12:02:41 -0600

    Finished intial baxter house styling

--- src/views/FacilitiesView.vue
+++ src/views/FacilitiesView.vue
@@ -1,14 +1,25 @@
 <template>
-  <section class="text-6xl border-b-2 border-b-brand-green-gray">
+  <section class="text-6xl border-y-2 border-y-brand-green-gray py-4">
     Facilities
+  </section>
+  <section class="grid grid-rows-2 mt-10">
+    <div class="row-start-1 row-span-1 flex justify-evenly">
+      <display-card-baxter-house class="w-5/6 md:w-2/7" />
+      <display-card-baxter-house class="w-5/6 md:w-2/7" />
+      <display-card-baxter-house class="w-5/6 md:w-2/7" />
+    </div>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import DisplayCardBaxterHouse from "@/components/Facilities/Cards/DisplayCardBaxterHouse.vue";
 
 export default defineComponent({
   name: "FacilitiesView",
+  components: {
+    DisplayCardBaxterHouse,
+  },
   setup() {},
 });
 </script>
