71c9245813209360315a9271c9b4df040d7cadcf
peytonhobson <peyton.hobson1@gmail.com>
2022-10-02 08:55:40 -0600

    Changed facilities desktop to be row of four cards

--- src/views/FacilitiesView.vue
+++ src/views/FacilitiesView.vue
@@ -2,21 +2,15 @@
   <section class="text-6xl border-y-2 border-y-brand-green-gray py-4">
     Facilities
   </section>
-  <section v-if="!isMobile" class="grid grid-rows-2 my-10">
-    <div class="row-start-1 row-span-1 flex justify-evenly">
-      <display-card-baxter-house class="w-5/6 md:w-2/7 show-on-scroll" />
-      <display-card-hallet-house class="w-5/6 md:w-2/7 show-on-scroll" />
-      <display-card-juneau-house class="w-5/6 md:w-2/7 show-on-scroll" />
-    </div>
-    <div class="row-start-2 row-span-1 flex justify-center mt-10">
-      <display-card-madrona-house class="w-5/6 md:w-2/7 show-on-scroll mx-10" />
-      <display-card-trapper-house class="w-5/6 md:w-2/7 show-on-scroll mx-10" />
-    </div>
+  <section v-if="!isMobile" class="flex flex-wrap justify-evenly my-10">
+    <display-card-baxter-house class="w-5/6 md:w-1/5 show-on-scroll" />
+    <display-card-hallet-house class="w-5/6 md:w-1/5 show-on-scroll" />
+    <display-card-madrona-house class="w-5/6 md:w-1/5 show-on-scroll" />
+    <display-card-trapper-house class="w-5/6 md:w-1/5 show-on-scroll" />
   </section>
   <section v-else class="flex flex-wrap justify-evenly my-10">
     <display-card-baxter-house class="w-5/6 md:w-2/7 show-on-scroll" />
     <display-card-hallet-house class="w-5/6 md:w-2/7 show-on-scroll mt-5" />
-    <display-card-juneau-house class="w-5/6 md:w-2/7 show-on-scroll mt-5" />
     <display-card-madrona-house class="w-5/6 md:w-2/7 show-on-scroll mt-5" />
     <display-card-trapper-house class="w-5/6 md:w-2/7 show-on-scroll mt-5" />
   </section>
@@ -27,7 +21,6 @@ import { defineComponent, onMounted } from "vue";
 
 import DisplayCardBaxterHouse from "@/components/Facilities/Cards/DisplayCardBaxterHouse.vue";
 import DisplayCardHalletHouse from "@/components/Facilities/Cards/DisplayCardHalletHouse.vue";
-import DisplayCardJuneauHouse from "@/components/Facilities/Cards/DisplayCardJuneauHouse.vue";
 import DisplayCardMadronaHouse from "@/components/Facilities/Cards/DisplayCardMadronaHouse.vue";
 import DisplayCardTrapperHouse from "@/components/Facilities/Cards/DisplayCardTrapperHouse.vue";
 import setScrollObserver from "@/helpers/setScrollObserver";
@@ -37,7 +30,6 @@ export default defineComponent({
   components: {
     DisplayCardBaxterHouse,
     DisplayCardHalletHouse,
-    DisplayCardJuneauHouse,
     DisplayCardMadronaHouse,
     DisplayCardTrapperHouse,
   },
