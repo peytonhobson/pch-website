8d7b10716d9ad6abe038d7f4699ead52e54aad88
peytonhobson <peyton.hobson1@gmail.com>
2022-10-02 15:39:28 -0600

    Added content to blog posts

---
+++ src/components/Blog/BlogPost.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="flex flex-col md:flex-row w-full lg:w-10/12">
+    <div class="md:mr-4 mb-2 md:mb-0 md:w-4/12">
+      <a class="bg-gray-100" href="/blog/2018-12-29-writing-with-markdown/">
+        <img
+          class="rounded mb-3 hover:opacity-70 transition duration-300 ease-in-out w-full h-full"
+          alt="Writing With Markdown"
+          :src="blogContent.mainImage"
+      /></a>
+    </div>
+
+    <div class="flex-1">
+      <div class="flex items-center">
+        <div class="flex ml-4">
+          <div class="flex items-center mr-2 -ml-4">
+            <img
+              class="rounded-full border-white border-2"
+              src="@/assets/Headshot.jpg"
+              alt="Sage Kirk"
+              width="28"
+              height="28"
+            />
+          </div>
+        </div>
+
+        <div class="text-sm font-medium text-gray-700">
+          {{ blogContent.date }}
+        </div>
+      </div>
+
+      <a
+        href="/blog/2018-12-29-writing-with-markdown/"
+        class="hover:text-brand-green-gray"
+      >
+        <h2 class="text-2xl text-left font-semibold mb-1">
+          {{ blogContent.title }}
+        </h2>
+      </a>
+
+      <p class="text-base text-left font-light text-gray-600 mb-4">
+        {{ blogContent.description }}
+      </p>
+
+      <div class="mb-2 flex items-start justify-start">
+        <router-link
+          to="/"
+          :key="category"
+          v-for="category in blogContent.categories"
+          class="p-1 px-3 mr-1 mb-1 inline-block text-xs font-mono rounded bg-green-200 text-green-800 hover:bg-blue-200 hover:text-blue-800 transition duration-300 ease-in-out"
+          >{{ category }}</router-link
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface BlogContent {
+  mainImage: string;
+  authorImage: string;
+  date: string;
+  title: string;
+  description: string;
+  categories: string[];
+}
+
+export default defineComponent({
+  name: "BlogPost",
+  props: {
+    blogContent: {
+      type: Object as () => BlogContent,
+      required: true,
+    },
+  },
+  setup(props) {},
+});
+</script>
--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,77 +1,16 @@
 <template>
+  <section class="text-6xl border-y-2 border-y-brand-green-gray py-4">
+    Blog
+  </section>
   <div class="container mx-auto md:px-4">
     <main class="py-6">
-      <div class="mb-12 flex items-start justify-start">
-        <div class="flex flex-col md:flex-row w-full lg:w-10/12">
-          <div class="md:mr-4 mb-2 md:mb-0 md:w-4/12">
-            <a
-              class="bg-gray-100"
-              href="/blog/2018-12-29-writing-with-markdown/"
-            >
-              <img
-                class="rounded mb-3 hover:opacity-70 transition duration-300 ease-in-out"
-                alt="Writing With Markdown"
-                src="https://jekyll-atlantic.netlify.app/assets/images/unsplash-CTivHyiTbFw-640x360.jpeg"
-                width="640"
-                height="360"
-            /></a>
-          </div>
-
-          <div class="flex-1">
-            <div class="flex items-center">
-              <div class="flex ml-4">
-                <div class="flex items-center mr-2 -ml-4">
-                  <img
-                    class="rounded-full border-white border-2"
-                    src="@/assets/Headshot.jpg"
-                    alt="Sage Kirk"
-                    width="28"
-                    height="28"
-                  />
-                </div>
-              </div>
-
-              <div class="text-sm font-medium text-gray-700">
-                26 February 2015
-              </div>
-            </div>
-
-            <a
-              href="/blog/2018-12-29-writing-with-markdown/"
-              class="hover:text-brand-green-gray"
-            >
-              <h2 class="text-2xl text-left font-semibold mb-1">
-                Important Information for those (All too frequent) Pharmacy
-                Visits
-              </h2>
-            </a>
-
-            <p class="text-base text-left font-light text-gray-600 mb-4">
-              Typically, as we age, we tend to end up taking more and more
-              medication. Sometimes these medicines are over the counter...
-            </p>
-
-            <div class="mb-2 flex items-start justify-start">
-              <a
-                class="p-1 px-3 mr-1 mb-1 inline-block text-xs font-mono rounded bg-green-200 text-green-800 hover:bg-blue-200 hover:text-blue-800 transition duration-300 ease-in-out"
-                href="/category/development"
-                >Premier</a
-              >
-
-              <a
-                class="p-1 px-3 mr-1 mb-1 inline-block text-xs font-mono rounded bg-green-200 text-green-800 hover:bg-blue-200 hover:text-blue-800 transition duration-300 ease-in-out"
-                href="/category/javascript"
-                >Care</a
-              >
-
-              <a
-                class="p-1 px-3 mr-1 mb-1 inline-block text-xs font-mono rounded bg-green-200 text-green-800 hover:bg-blue-200 hover:text-blue-800 transition duration-300 ease-in-out"
-                href="/category/web-design"
-                >Homes</a
-              >
-            </div>
-          </div>
-        </div>
+      <div class="mb-12 flex items-start justify-start flex-wrap">
+        <blog-post
+          :key="blogContent.date"
+          v-for="blogContent in blogContents"
+          :blogContent="blogContent"
+          class="py-5 border-y"
+        />
       </div>
     </main>
   </div>
@@ -79,13 +18,55 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Paginator from "@/components/Blog/Paginator.vue";
+import BlogPost from "@/components/Blog/BlogPost.vue";
 
 export default defineComponent({
   name: "BlogView",
   components: {
-    Paginator,
+    BlogPost,
   },
-  setup() {},
+  setup() {
+    const blogContents = [
+      {
+        mainImage: "https://source.unsplash.com/INHtWKpBTsA/640x360",
+        authorImage: "src/assets/Headshot.jpg",
+        date: "26 February 2015",
+        title:
+          "Important Information for those (All too frequent) Pharmacy Visits",
+        description:
+          "Typically, as we age, we tend to end up taking more and more medication. Sometimes these medicines are over the counter... ",
+        categories: ["Medication", "Dealing with Age", "Pharmacy"],
+      },
+      {
+        mainImage: "https://source.unsplash.com/y3PdLg_zSDs/640x360",
+        authorImage: "src/assets/Headshot.jpg",
+        date: "27 January 2015",
+        title: "Osteoporosis",
+        description:
+          "I enjoy continuing my education and staying up to date on health related news, especially as it relates to the senior population.",
+        categories: ["Bone Health", "Education"],
+      },
+      {
+        mainImage: "https://source.unsplash.com/YmQ0-nmWcV0/640x360",
+        authorImage: "src/assets/Headshot.jpg",
+        date: "13 January 2015",
+        title: "Let’s Help Recall Those Memories",
+        description:
+          "Selfishly, we all get a little upset and sad when our elderly loved ones don’t recognize us or recall some of our favorite memories.  ",
+        categories: ["Dimentia", "Loved Ones", "Memories"],
+      },
+      {
+        mainImage: "https://source.unsplash.com/MS7KD9Ti7FQ/640x360",
+        authorImage: "src/assets/Headshot.jpg",
+        date: "20 November 2014",
+        title: "Maybe They Wont Have to Turn up That Hearing Aid",
+        description:
+          "I think most of us can agree that many of our elderly family members are hard of hearing, or are certainly headed in that direction.",
+        categories: ["Hearing", "Family"],
+      },
+    ];
+
+    return { blogContents };
+  },
 });
 </script>
