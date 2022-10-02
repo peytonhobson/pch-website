6ca2cfc70aa06c5aaa7653a0eaf0a1c6fdffd8c2
peytonhobson <peyton.hobson1@gmail.com>
2022-10-02 14:48:43 -0600

    Added blogview to route

---
+++ src/views/BlogView.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="py-6">
+    <paginator />
+    <div class="mb-12">{% include summary.html post=post %}</div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Paginator from "@/components/Blog/Paginator.vue";
+
+export default defineComponent({
+  name: "BlogView",
+  components: {
+    Paginator,
+  },
+  setup() {},
+});
+</script>
