8dca90253b1486ef74eb3b14bab92efbebe275c6
peytonhobson <peyton.hobson1@gmail.com>
2022-10-03 09:51:29 -0600

    Changed URL for production to include s3 bucket

--- src/api/getPosts.ts
+++ src/api/getPosts.ts
@@ -4,7 +4,8 @@ import { Post } from "@/api/types";
 
 const getPosts = async () => {
   const baseUrl = process.env.VUE_APP_API_URL;
-  const response = await axios.get<Post[]>(`${baseUrl}/posts`);
+  const path = process.env.VUE_APP_DATA_PATH;
+  const response = await axios.get<Post[]>(`${baseUrl}${path}`);
   return response.data;
 };
 
