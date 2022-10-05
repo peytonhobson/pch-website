5f3d3413096ccaeb43f31960e418d4f5023dfd73
peytonhobson <peyton.hobson1@gmail.com>
2022-10-05 06:46:43 -0600

    Added images to testimonials and prepped for mobile

--- src/views/TestimonialsView.vue
+++ src/views/TestimonialsView.vue
@@ -7,25 +7,44 @@
     class="py-5 grid grid-cols-3"
   >
     <div class="flex flex-wrap col-span-1 col-start-1 justify-center">
-      <testimonial-card :testimonial="testimonials[0]" class="my-5 md:w-5/6" />
+      <div
+        class="h-80 bg-hero-image-trapper bg-cover my-5 md:w-5/6 rounded-2xl"
+      ></div>
+      <testimonial-card :testimonial="testimonials[3]" class="my-5 md:w-5/6" />
       <testimonial-card :testimonial="testimonials[1]" class="my-5 md:w-5/6" />
     </div>
     <div class="flex flex-wrap col-span-1 col-start-2 justify-center">
       <testimonial-card :testimonial="testimonials[2]" class="my-5 md:w-5/6" />
+      <div
+        class="h-80 bg-hero-image-madrona bg-cover my-5 md:w-5/6 rounded-2xl"
+      ></div>
       <testimonial-card :testimonial="testimonials[5]" class="my-5 md:w-5/6" />
     </div>
     <div class="flex flex-wrap col-span-1 col-start-3 justify-center">
       <testimonial-card :testimonial="testimonials[4]" class="my-5 md:w-5/6" />
-      <testimonial-card :testimonial="testimonials[3]" class="my-5 md:w-5/6" />
+      <testimonial-card :testimonial="testimonials[0]" class="my-5 md:w-5/6" />
+      <div
+        class="h-80 bg-hero-image-don bg-cover my-5 md:w-5/6 rounded-2xl"
+      ></div>
     </div>
   </section>
-  <section v-if="isMobile" class="py-5 flex flex-wrap justify-center">
-    <testimonial-card
-      v-for="testimonial in testimonials"
-      :key="testimonial.id"
-      :testimonial="testimonial"
-      class="my-5 w-5/6"
-    />
+  <section
+    v-if="testimonials.length > 0 && isMobile"
+    class="py-5 flex flex-wrap justify-center"
+  >
+    <testimonial-card :testimonial="testimonials[0]" class="my-5 w-5/6" />
+    <div
+      class="h-80 bg-hero-image-trapper bg-cover my-5 w-5/6 rounded-2xl"
+    ></div>
+    <testimonial-card :testimonial="testimonials[1]" class="my-5 w-5/6" />
+    <testimonial-card :testimonial="testimonials[2]" class="my-5 w-5/6" />
+    <div
+      class="h-80 bg-hero-image-madrona bg-cover my-5 w-5/6 rounded-2xl"
+    ></div>
+    <testimonial-card :testimonial="testimonials[3]" class="my-5 w-5/6" />
+    <testimonial-card :testimonial="testimonials[4]" class="my-5 w-5/6" />
+    <testimonial-card :testimonial="testimonials[5]" class="my-5 w-5/6" />
+    <div class="h-80 bg-hero-image-don bg-cover my-5 w-5/6 rounded-2xl"></div>
   </section>
 </template>
 
