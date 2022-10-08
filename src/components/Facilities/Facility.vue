e3928480992df5c6c7d59dffa8885e7bca737dfa
peytonhobson <peyton.hobson1@gmail.com>
2022-10-08 16:46:44 -0600

    Added changing images to facilities

--- src/components/Facilities/Facility.vue
+++ src/components/Facilities/Facility.vue
@@ -1,14 +1,18 @@
 <template>
   <div
-    v-if="facility"
+    v-if="facility && imagesLoaded"
     class="mt-5 md:mt-10 h-128 md:px-20 motion-safe:animate-fadeIn"
   >
     <div class="grid md:grid-cols-12 grid-cols-1 h-full justify-evenly">
       <div class="md:col-span-5 h-40 md:h-full">
         <!-- Fix later for dynamic image and production link -->
-        <div
-          class="md:relative h-full flex items-center justify-center md:rounded-xl shadow-md bg-cover"
-        ></div>
+        <Transition>
+          <div
+            v-if="show"
+            class="md:relative h-full flex items-center justify-center md:rounded-xl shadow-md bg-cover"
+            :style="`background-image: url(${currentImage})`"
+          ></div>
+        </Transition>
       </div>
       <!--end col-->
 
@@ -55,10 +59,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs } from "vue";
+import {
+  defineComponent,
+  toRefs,
+  computed,
+  ref,
+  onBeforeMount,
+  onBeforeUnmount,
+} from "vue";
 
 import { Facility } from "@/api/types";
 import ActionButton from "../Shared/ActionButton.vue";
+import preloadImages from "@/helpers/preloadImages";
 
 export default defineComponent({
   name: "Facility",
@@ -78,12 +90,39 @@ export default defineComponent({
       window.open(facility.value.location.replace("&output=embed", ""));
     };
 
-    return { routeUser };
+    const curIndex = ref(0);
+    const currentImage = computed(() => facility.value.images[curIndex.value]);
+
+    const show = ref(true);
+    let interval: any = null;
+    const changeFacility = () => {
+      interval = setInterval(() => {
+        show.value = !show.value;
+        curIndex.value = (curIndex.value + 1) % facility.value.images.length;
+        setTimeout(() => (show.value = !show.value), 1000);
+      }, 6000);
+    };
+
+    const clearFacilityInterval = () => {
+      clearInterval(interval);
+    };
+
+    onBeforeMount(changeFacility);
+    onBeforeUnmount(clearFacilityInterval);
+
+    const imagesLoaded = ref(false);
+
+    onBeforeMount(
+      async () =>
+        (imagesLoaded.value = await preloadImages(facility.value.images))
+    );
+
+    return { routeUser, show, currentImage, imagesLoaded };
   },
 });
 </script>
 
-<style>
+<style scoped>
 .mapouter {
   position: relative;
   text-align: right;
@@ -92,6 +131,16 @@ export default defineComponent({
 .gmap_canvas {
   overflow: hidden;
   background: none !important;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
 >
