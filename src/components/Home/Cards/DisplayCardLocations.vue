01619b6c5ead9ddeb4999cb183af64b723bb1c02
peytonhobson <peyton.hobson1@gmail.com>
2022-10-08 12:40:43 -0600

    Changed locations card to fit mobile

--- src/components/Home/Cards/DisplayCardLocations.vue
+++ src/components/Home/Cards/DisplayCardLocations.vue
@@ -1,10 +1,10 @@
 <template>
   <display-card header="Locations" :route="route">
     <div class="row-start-2 row-span-4">
-      <section class="w-full row-start-2 row-span-2">
+      <section class="w-full h-1/2">
         <img src="@/assets/locations-teaser.png" class="w-full h-full" />
       </section>
-      <section class="row-start-4 row-span-3 grid grid-rows-4">
+      <section class="grid grid-rows-4">
         <section class="p-7 row-start-1 row-span-3 m-auto">
           <div class="text-lg font-bold text-left mb-3 font-sen">Locations</div>
           <div class="text-base md:text-lg text-left font-sans">
