2c10cf2860437a6538384063b2676ad62cd10105
peytonhobson <peyton.hobson1@gmail.com>
2022-10-17 11:44:11 -0600

    Added embed to locations

--- src/views/LocationsView.vue
+++ src/views/LocationsView.vue
@@ -1,12 +1,15 @@
 <template>
-  <iframe
-    src="https://storage.googleapis.com/maps-solutions-vc5lhmrtc4/locator-plus/4rmr/locator-plus.html"
-    width="75%"
-    height="75%"
-    style="border: 0"
-    loading="lazy"
-  >
-  </iframe>
+  <div class="h-[12vh]"></div>
+  <main class="flex h-screen w-full items-start justify-center">
+    <iframe
+      src="https://storage.googleapis.com/maps-solutions-vc5lhmrtc4/locator-plus/4rmr/locator-plus.html"
+      width="75%"
+      height="75%"
+      style="border: 0"
+      loading="lazy"
+    >
+    </iframe>
+  </main>
 </template>
 
 <script lang="ts">
