348a9c88280edc18ff4fc89aede4778832a46d32
peytonhobson <peyton.hobson1@gmail.com>
2022-10-18 10:28:38 -0600

    Added green background to home cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,31 +11,33 @@
       /></router-link></div
   ></hero>
   <hero-caption />
-  <div class="flex flex-wrap w-full justify-evenly show-on-scroll">
-    <description-box
-      v-for="descriptionBoxContent in descriptionBoxContents"
-      :key="descriptionBoxContent.title"
-      :icon="descriptionBoxContent.icon"
-      :title="descriptionBoxContent.title"
-      :body="descriptionBoxContent.body"
-      :icon-class="descriptionBoxContent.iconClass"
-      class="md:max-w-md md:w-2/5 xl:w-2/7 mt-10 w-5/6 h-72"
-    />
-  </div>
-  <div
-    class="flex flex-wrap justify-evenly md:py-10 pb-10 show-on-scroll carousel-tag"
-  >
-    <facilities-card
-      class="md:max-w-md md:w-2/5 xl:w-2/7 mt-10 md:my-10 w-5/6 h-144"
-    />
-    <locations-card
-      class="md:max-w-md md:w-2/5 xl:w-2/7 mt-10 md:my-10 w-5/6 h-144"
-      route="Facilities"
-    />
-    <contact-card
-      class="md:max-w-md md:w-2/5 mt-10 xl:w-2/7 md:my-10 w-5/6 h-144"
-    />
-  </div>
+  <section class="bg-brand-green-gray">
+    <div class="flex flex-wrap w-full justify-evenly show-on-scroll">
+      <description-box
+        v-for="descriptionBoxContent in descriptionBoxContents"
+        :key="descriptionBoxContent.title"
+        :icon="descriptionBoxContent.icon"
+        :title="descriptionBoxContent.title"
+        :body="descriptionBoxContent.body"
+        :icon-class="descriptionBoxContent.iconClass"
+        class="md:max-w-md md:w-2/5 xl:w-2/7 mt-10 w-5/6 h-72"
+      />
+    </div>
+    <div
+      class="flex flex-wrap justify-evenly md:py-10 pb-10 show-on-scroll carousel-tag"
+    >
+      <facilities-card
+        class="md:max-w-md md:w-2/5 xl:w-2/7 mt-10 md:my-10 w-5/6 h-144"
+      />
+      <locations-card
+        class="md:max-w-md md:w-2/5 xl:w-2/7 mt-10 md:my-10 w-5/6 h-144"
+        route="Facilities"
+      />
+      <contact-card
+        class="md:max-w-md md:w-2/5 mt-10 xl:w-2/7 md:my-10 w-5/6 h-144"
+      />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -49,6 +51,8 @@ import LocationsCard from "@/components/Home/Cards/LocationsCard.vue";
 import ContactCard from "@/components/Home/Cards/ContactCard/ContactCard.vue";
 import setScrollObserver from "@/composables/setScrollObserver";
 import ActionButton from "@/components/Shared/ActionButton.vue";
+
+import getImgURL from "@/composables/getImgURL";
 
 interface DescriptionBoxContent {
   icon: string[];
@@ -90,15 +94,9 @@ export default defineComponent({
       },
     ];
 
-    const imageLinks = [
-      "other/resident-edith-and-red.jpg",
-      "other/resident-blue-tag-dog.png",
-      "madrona/madrona-2.jpg",
-    ];
-
     onMounted(setScrollObserver);
 
-    return { descriptionBoxContents, imageLinks };
+    return { descriptionBoxContents, getImgURL };
   },
 });
 </script>
