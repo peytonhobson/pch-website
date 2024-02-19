f3e5bffd597e247eb4b1c722235b9535ce48a24e
peytonhobson <peyton.hobson1@gmail.com>
2024-02-19 12:58:54 -0700

    Updated deps

--- src/composables/useConfirmRoute.ts
+++
@@ -1,9 +0,0 @@
-import { computed } from "vue";
-import { useRoute } from "vue-router";
-
-const useConfirmRoute = (routeName: string) => {
-  const route = useRoute();
-  return computed(() => route.name === routeName);
-};
-
-export default useConfirmRoute;
